610faad070e78bf495f18fded8b20b44c38a762f
Rakesh <134880267+rakesh-nandakumar@users.noreply.github.com>
2025-11-17 12:11:17 +0530

    init

--- StudentManagementSystem - Copy (5)/obj/Debug/net8.0-windows/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem - Copy (5)/obj/Debug/net8.0-windows/StudentManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507ad8ff4bd1493a4a9618cc2a3423b9ca017e3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
